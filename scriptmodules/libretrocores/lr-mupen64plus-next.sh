7c05c7b7e33da2b1b5f47baa869ca9578fcf8c2e
rearm <69484060+rearmit@users.noreply.github.com>
2020-12-17 17:24:42 +0100

    Update lr-mupen64plus-next.sh

--- scriptmodules/libretrocores/lr-mupen64plus-next.sh
+++ scriptmodules/libretrocores/lr-mupen64plus-next.sh
@@ -38,7 +38,7 @@ function build_lr-mupen64plus-next() {
             params+=(platform="$__platform-mesa")
         elif isPlatform "mali"; then
             params+=(platform="odroid")
-        elif isPlatform "armv7-mali"; then
+        elif isPlatform "kms"; then
             params+=(platform="odroid")
         fi
         if isPlatform "neon"; then
